c25e87f40ca13f271946686e8f4692735f0d9a68
sigmotoa <sigmotoa@misena.edu.co>
2018-04-03 18:33:48 -0500

    primera carga

---
+++ app/src/main/java/com/sigmotoa/calcular/Calculadora.java
@@ -0,0 +1,16 @@
+package com.sigmotoa.calcular;
+
+/**
+ * Created by sigmotoa on 3/04/18.
+ */
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        Operaciones operar=new Operaciones();
+
+        System.out.println(operar.sumar(3.0,5.2));
+
+    }
+}
---
+++ app/src/main/java/com/sigmotoa/calcular/Operaciones.java
@@ -0,0 +1,18 @@
+package com.sigmotoa.calcular;
+
+/**
+ * Created by sigmotoa on 3/04/18.
+ */
+
+public class Operaciones {
+
+    public double sumar(double a, double b)
+    {
+        return a+b;
+    }
+
+    public int sumar(int a, int b)
+    {
+        return a+b;
+    }
+}
